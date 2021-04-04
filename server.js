885de2020011d17c04b5b90a09a5da8440c694f2
othernamesweretaken <47422838+othernamesweretaken@users.noreply.github.com>
2021-04-05 01:14:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const next = require('next')
 const routes = require('./routes')
-const app = next({dev: process.env.NODE_ENV !== 'production'})
+const app = next({dev: process.env.NODE_ENV == 'production'})
 const handler = routes.getRequestHandler(app)
  
  
 // Without express
-const {createServer} = require('http')
+const {createServer} = require('https')
 app.prepare().then(() => {
   createServer(handler).listen(3000,(err)=>{
     if(err) throw err;
